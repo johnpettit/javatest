576dcc84223a1f6663ea37906fa236dedcbdaee6
John Pettit <john.pettit@tridonic.com>
2015-11-07 06:31:25 -0800

    New stuff

---
+++ src/main/java/test/Game.java
@@ -0,0 +1,39 @@
+package test;
+
+public class Game {
+
+	private String name;
+	private String version;
+	
+	public void startGame() {
+		System.out.println("Starting Game");
+	}	
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+	/**
+	 * @param version the version to set
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
+}
--- src/main/java/test/Start.java
+++ src/main/java/test/Start.java
@@ -5,6 +5,10 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD!");
+		
+		Game game = new Game();
+		
+		game.startGame();
 	}
 
 }
